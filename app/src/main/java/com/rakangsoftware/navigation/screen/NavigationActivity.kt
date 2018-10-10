cfd0986daac68008ee6fbfcc2f3ea1c8b02767b6
Per-Erik Bergman <bergman@uncle.se>
2018-10-10 18:48:41 +0200

    Projected added.

---
+++ app/src/main/java/com/rakangsoftware/navigation/screen/NavigationActivity.kt
@@ -0,0 +1,13 @@
+package com.rakangsoftware.navigation.screen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.rakangsoftware.navigation.R
+
+class NavigationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.navigation_activity)
+    }
+}
